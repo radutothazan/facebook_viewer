a3956c154137def0d47af28cdd338518f9069a32
Radu Tothazan <radutothazan@Radus-iMac.local>
2016-04-10 18:12:20 +0300

    Instalare si setare Cocoapods, adaugare custom log in with facebook button

--- Facebook Viewer/ViewController.swift
+++ Facebook Viewer/ViewController.swift
@@ -7,12 +7,30 @@
 //
 
 import UIKit
+import FBSDKLoginKit
+import FBSDKCoreKit
+
+
 
 class ViewController: UIViewController {
 
+    var dict : NSDictionary!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let accessToken = FBSDKAccessToken.currentAccessToken()
+        
+        if (accessToken == nil)
+        {
+            print("Not loggen in")
+        }
+        else
+        {
+            print("Logged in..")
+        }
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +38,38 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    @IBAction func loginAction(sender: AnyObject) {
+        
+        let fbLoginManager: FBSDKLoginManager = FBSDKLoginManager()
+        fbLoginManager.logInWithReadPermissions(["email"], handler: { (result, error)-> Void in
+            if error == nil
+            {
+                let fbLoginResult:FBSDKLoginManagerLoginResult = result
+                if fbLoginResult.grantedPermissions.contains("email")
+                {
+                    self.getFBUserData()
+                }
+            }
+        })
+    }
+    
+    func getFBUserData(){
+        if FBSDKAccessToken.currentAccessToken() != nil
+        {
+            FBSDKGraphRequest(graphPath: "me", parameters: ["fields": "id, name, first_name, last_name, picture.type(large), email"]).startWithCompletionHandler({ (connection, result, error) -> Void in
+                if error == nil
+                {
+                    self.dict = result as! NSDictionary
+                    print(result)
+                    print(self.dict)
+                    NSLog(self.dict.objectForKey("picture")?.objectForKey("data")?.objectForKey("url") as! String)
+                }
+            })
+        }
+    }
+    
+    
 
 }
 
---
+++ Podfile
@@ -0,0 +1,14 @@
+# Uncomment this line to define a global platform for your project
+platform :ios, '8.0'
+# Uncomment this line if you're using Swift
+use_frameworks!
+
+pod 'Bolts'
+pod 'FBSDKCoreKit'
+pod 'FBSDKShareKit'
+pod 'FBSDKLoginKit'
+
+target 'Facebook Viewer' do
+
+end
+
